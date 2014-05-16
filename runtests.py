1b19c04ac880dc1b16919bb191ad03bb95eb3093
Tom Christie <tom@tomchristie.com>
2014-05-16 16:10:27 +0100

    Nicer runtests style.

--- runtests.py
+++ runtests.py
@@ -7,15 +7,11 @@ import subprocess
 
 PYTEST_ARGS = {
     'default': ['tests', '--cov', 'core_serializers', '--cov', 'tests', '--cov-report', 'term', '--cov-report', 'html'],
-    'simple': ['tests', '-q'],
     'travis': ['tests', '--cov', 'core_serializers', '--cov', 'tests', '--cov-report', 'term', '-v']
 }
 
-FLAKE8_ARGS = {
-    'default': ['core_serializers', 'tests', '--ignore=E501'],
-    'simple': ['core_serializers', 'tests', '--ignore=E501'],
-    'travis': ['core_serializers', 'tests', '--ignore=E501'],
-}
+FLAKE8_ARGS = ['core_serializers', 'tests', '--ignore=E501']
+
 
 sys.path.append(os.path.dirname(__file__))
 
@@ -28,11 +24,12 @@ def flake8_main(args):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1 and sys.argv[1] in PYTEST_ARGS.keys():
-        style = sys.argv[1]
-        custom_args = sys.argv[2:]
+    if len(sys.argv) > 1:
+        pytest_args = sys.argv[1:]
+        if not pytest_args or pytest_args[0].startswith('-'):
+            pytest_args = ['tests'] + pytest_args
     else:
-        style = 'default'
-        custom_args = sys.argv[1:]
-    exit_on_failure(pytest.main(PYTEST_ARGS[style] + custom_args))
-    exit_on_failure(flake8_main(FLAKE8_ARGS[style]))
+        pytest_args = PYTEST_ARGS['default']
+
+    exit_on_failure(pytest.main(pytest_args))
+    exit_on_failure(flake8_main(FLAKE8_ARGS))
