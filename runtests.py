9784ccb24b77f8f9fd8003d034f640998f1f61b3
Tom Christie <tom@tomchristie.com>
2014-05-19 11:59:29 +0100

    runtests.py accepts function and/or class name

--- runtests.py
+++ runtests.py
@@ -22,12 +22,28 @@ def exit_on_failure(ret):
 def flake8_main(args):
     return subprocess.call(['flake8'] + args)
 
+def split_class_and_function(string):
+    class_string, function_string = string.split('.', 1)
+    return "%s and %s" % (class_string, function_string)
+
+def is_function(string):
+    return string.startswith('test_') or '.test_' in string
+
+def is_class(string):
+    return string[0] == string[0].upper()
+
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         pytest_args = sys.argv[1:]
-        if not pytest_args or pytest_args[0].startswith('-'):
+        first_arg = pytest_args[0]
+        if first_arg.startswith('-'):
             pytest_args = ['tests'] + pytest_args
+        elif is_class(first_arg) and is_function(first_arg):
+            expression = split_class_and_function(first_arg)
+            pytest_args = ['tests', '-k', expression] + pytest_args[1:]
+        elif is_class(first_arg) or is_function(first_arg):
+            pytest_args = ['tests', '-k', pytest_args[0]] + pytest_args[1:]
     else:
         pytest_args = PYTEST_ARGS['default']
 
